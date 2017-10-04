401fcf524cd4ac127d88f95a8b345ae2a8f0b201
Korneliusz Kirsz <kkirsz@divante.pl>
2017-10-04 13:26:15 +0200

    JA:SOD-66:Adding skeleton css sheets & changes in configuration view

--- Resources/views/Config/index.html.php
+++ Resources/views/Config/index.html.php
@@ -5,30 +5,44 @@
  * @author      Korneliusz Kirsz <kkirsz@divante.pl>
  * @copyright   Copyright (c) 2017 DIVANTE (http://divante.pl)
  */
+
+$this->extend('DivanteTrackingBundle::layout.html.php');
 ?>
-<h4>Divante Tracking Bundle Configuration</h4>
-<form action="<?= $this->path('divante_tracking_config_save') ?>" method="post">
-    <div>
-        MongoDB username:
-        <input type="text" name="username" value="<?= $this->escape($this->config['username']) ?>" style="width:100%;" />
-    </div>
-    <div>
-        MongoDB password:
-        <input type="text" name="password" value="<?= $this->escape($this->config['password']) ?>" style="width:100%;" />
-    </div>
-    <div>
-        MongoDB host:
-        <input type="text" name="host" value="<?= $this->escape($this->config['host']) ?>" style="width:100%;" />
-    </div>
-    <div>
-        MongoDB port:
-        <input type="text" name="port" value="<?= $this->escape($this->config['port']) ?>" style="width:100%;" />
-    </div>
-    <div>
-        MongoDB database:
-        <input type="text" name="database" value="<?= $this->escape($this->config['database']) ?>" style="width:100%;" />
-    </div>
-    <div>
-        <input type="submit" value="Submit" />
-    </div>
-</form>
+<div class="container" style="margin-top: 5%;">
+    <h4>Divante Tracking Bundle Configuration</h4>
+    <form action="<?= $this->path('divante_tracking_config_save') ?>" method="post">
+        <div class="row">
+            <div class="six columns">
+                <label for="username">MongoDB username</label>
+                <input id="username" class="u-full-width" type="text" name="username" value="<?= $this->escape($this->config['username']) ?>">
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <label for="password">MongoDB password</label>
+                <input id="password" class="u-full-width" type="text" name="password" value="<?= $this->escape($this->config['password']) ?>">
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <label for="host">MongoDB host</label>
+                <input id="host" class="u-full-width" type="text" name="host" value="<?= $this->escape($this->config['host']) ?>">
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <label for="port">MongoDB port</label>
+                <input id="port" class="u-full-width" type="text" name="port" value="<?= $this->escape($this->config['port']) ?>">
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <label for="database">MongoDB database</label>
+                <input id="database" class="u-full-width" type="text" name="database" value="<?= $this->escape($this->config['database']) ?>">
+            </div>
+        </div>
+        <div>
+            <input class="button-primary" type="submit" value="Submit">
+        </div>
+    </form>
+</div>
---
+++ Resources/views/layout.html.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @category    pimcore5-user-tracking
+ * @date        04/10/2017
+ * @author      Korneliusz Kirsz <kkirsz@divante.pl>
+ * @copyright   Copyright (c) 2017 DIVANTE (http://divante.pl)
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Divante Tracking Bundle Configuration</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/bundles/divantetracking/css/skeleton/normalize.css">
+    <link rel="stylesheet" href="/bundles/divantetracking/css/skeleton/skeleton.css">
+</head>
+<body>
+    <?php $this->slots()->output('_content'); ?>
+</body>
+</html>
